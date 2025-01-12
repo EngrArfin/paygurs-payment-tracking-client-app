e7cfa0e46e678c3fb07fd7a1253a6dfd56b9cee0
EngrArfin <arfin.cse.green.edu.bd@gmail.com>
2025-01-13 03:00:51 +0600

    shadcn setup with button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <div className="h-screen flex text-5xl justify-center ">
       <h1>Payment Track App</h1>
+      <Button>Click me</Button>
     </div>
   );
 }
