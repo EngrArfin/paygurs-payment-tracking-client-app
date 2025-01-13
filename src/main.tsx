7ad61c95feae5974994d5b148c16421f3ee5b041
EngrArfin <arfin.cse.green.edu.bd@gmail.com>
2025-01-13 13:14:49 +0600

    redux setup

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { Provider } from "react-redux";
+import { store } from "./redux/feature/store.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <div className="bg-red-300 ">
     <React.StrictMode>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </React.StrictMode>
   </div>
 );
